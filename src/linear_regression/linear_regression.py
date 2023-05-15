6b7565ff0763e63ae54faa5286a9015c62eefb66
Brainiac <stefan.micic@topta.com>
2023-05-15 14:13:08 +0200

    github docker

--- src/linear_regression/linear_regression.py
+++ src/linear_regression/linear_regression.py
@@ -2,7 +2,7 @@ from pandas import Series
 from sklearn.model_selection import train_test_split
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-from src.utils import normalize_column
+from utils import normalize_column
 from sklearn import metrics
 import numpy as np
 from pandas import DataFrame
@@ -11,7 +11,12 @@ import pickle
 
 
 class LinearReg:
-    def __init__(self, path: str = "../data/regression/weatherHistory.csv", n_jobs: int = 10, model_path: str = "../models/trained.sav"):
+    def __init__(
+        self,
+        path: str = "data/regression/weatherHistory.csv",
+        n_jobs: int = 10,
+        model_path: str = "models/trained.sav",
+    ):
         """
         This is initializing the LinearReg class
 
@@ -24,12 +29,24 @@ class LinearReg:
         self.lm = LinearRegression(n_jobs=n_jobs)
         self.model_path = model_path
 
-    def preprocess(self, factorization_list: List[str] = ['Summary', 'Precip Type', 'Daily Summary'],
-                   columns: List[str] = ['Summary', 'Precip Type', 'Apparent Temperature (C)', 'Humidity', 'Wind Speed (km/h)',
-                                         'Wind Bearing (degrees)', 'Visibility (km)', 'Loud Cover',
-                                         'Pressure (millibars)', 'Daily Summary'],
-                   target: List[str] = ['Temperature (C)'],
-                   normalize_column_name: str = 'Pressure (millibars)') -> None:
+    def preprocess(
+        self,
+        factorization_list: List[str] = ["Summary", "Precip Type", "Daily Summary"],
+        columns: List[str] = [
+            "Summary",
+            "Precip Type",
+            "Apparent Temperature (C)",
+            "Humidity",
+            "Wind Speed (km/h)",
+            "Wind Bearing (degrees)",
+            "Visibility (km)",
+            "Loud Cover",
+            "Pressure (millibars)",
+            "Daily Summary",
+        ],
+        target: List[str] = ["Temperature (C)"],
+        normalize_column_name: str = "Pressure (millibars)",
+    ) -> None:
         """
         Preprocess the data by normalizing a column and factorizing columns
 
@@ -44,7 +61,8 @@ class LinearReg:
         X = self.df[columns]
         y = self.df[target]
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
-            X, y, test_size=0.2, random_state=101)
+            X, y, test_size=0.2, random_state=101
+        )
 
     def factorize(self, columns) -> None:
         """
@@ -63,7 +81,7 @@ class LinearReg:
 
         """
         trained = self.lm.fit(self.X_train, self.y_train)
-        pickle.dump(trained, open(self.model_path, 'wb'))
+        pickle.dump(trained, open(self.model_path, "wb"))
 
     def evaluate(self) -> int:
         """
@@ -73,9 +91,10 @@ class LinearReg:
         - metric: Root mean squared error.
 
         """
-        metric = np.sqrt(metrics.mean_squared_error(
-            self.y_test, self.predict(self.X_test)))
-        print(f'RMSE: {metric}')
+        metric = np.sqrt(
+            metrics.mean_squared_error(self.y_test, self.predict(self.X_test))
+        )
+        print(f"RMSE: {metric}")
         return metric
 
     def predict(self, X) -> LinearRegression:
--- tests/unit/test_lr.py
+++ tests/unit/test_lr.py
@@ -10,21 +10,23 @@ LOGGER = logging.getLogger(__name__)
 
 
 class TestLinearRegression(unittest.TestCase):
-
     @patch("pandas.read_csv")
     def test_preprocess(self, mock_reading):
-
         data = {
-            'A': [1, 2, 3, 4, 5],
-            'B': [7.388889, 10, 20, 1, 2],
-            'C': [1, 3, 4, 1, 2],
-            'D': [1, 40, 2, 4, 3]
+            "A": [1, 2, 3, 4, 5],
+            "B": [7.388889, 10, 20, 1, 2],
+            "C": [1, 3, 4, 1, 2],
+            "D": [1, 40, 2, 4, 3],
         }
         df = pd.DataFrame(data)
         mock_reading.return_value = df
         lin_reg_model = LinearReg()
-        lin_reg_model.preprocess(factorization_list=[], columns=[
-                                 'A', 'B', 'C'], target=['D'], normalize_column_name='B')
+        lin_reg_model.preprocess(
+            factorization_list=[],
+            columns=["A", "B", "C"],
+            target=["D"],
+            normalize_column_name="B",
+        )
 
         self.assertEqual(lin_reg_model.X_train.shape, (4, 3))
         self.assertEqual(lin_reg_model.y_train.shape, (4, 1))
@@ -33,30 +35,32 @@ class TestLinearRegression(unittest.TestCase):
 
     @patch("pandas.read_csv")
     def test_factorize_columns(self, mock_reading):
-        data = {'A': ['cloudy', 'sunny'], 'B': ['sunny', 'rainy']}
+        data = {"A": ["cloudy", "sunny"], "B": ["sunny", "rainy"]}
         df = pd.DataFrame(data)
         mock_reading.return_value = df
         lin_reg_model = LinearReg(df)
-        lin_reg_model.factorize(['A', 'B'])
+        lin_reg_model.factorize(["A", "B"])
 
-        self.assertEqual(list(lin_reg_model.df['A']), [1, 2])
-        self.assertEqual(list(lin_reg_model.df['B']), [1, 2])
+        self.assertEqual(list(lin_reg_model.df["A"]), [1, 2])
+        self.assertEqual(list(lin_reg_model.df["B"]), [1, 2])
 
     @patch("pandas.read_csv")
     def test_train_model(self, mock_reading):
         data = {
-            'A': [1, 2, 3, 4, 5],
-            'B': [7.388889, 10, 20, 1, 2],
-            'C': [1, 3, 4, 1, 2],
-            'D': [1, 40, 2, 4, 3]
+            "A": [1, 2, 3, 4, 5],
+            "B": [7.388889, 10, 20, 1, 2],
+            "C": [1, 3, 4, 1, 2],
+            "D": [1, 40, 2, 4, 3],
         }
         df = pd.DataFrame(data)
         mock_reading.return_value = df
         lin_reg_model = LinearReg(model_path="test-models/test.sav")
-        lin_reg_model.preprocess(factorization_list=[],
-                                 columns=['A', 'B', 'C'],
-                                 target=['D'],
-                                 normalize_column_name='B')
+        lin_reg_model.preprocess(
+            factorization_list=[],
+            columns=["A", "B", "C"],
+            target=["D"],
+            normalize_column_name="B",
+        )
         os.mkdir("test-models/")
         lin_reg_model.train_model()
         self.assertEqual(os.listdir("test-models/")[0], "test.sav")
@@ -65,20 +69,22 @@ class TestLinearRegression(unittest.TestCase):
     @patch("pandas.read_csv")
     def test_evaluate(self, mock_reading):
         data = {
-            'A': [1, 2, 3, 4, 5],
-            'B': [7.388889, 10, 20, 1, 2],
-            'C': [1, 3, 4, 1, 2],
-            'D': [1, 40, 2, 4, 3]
+            "A": [1, 2, 3, 4, 5],
+            "B": [7.388889, 10, 20, 1, 2],
+            "C": [1, 3, 4, 1, 2],
+            "D": [1, 40, 2, 4, 3],
         }
         df = pd.DataFrame(data)
         mock_reading.return_value = df
         lin_reg_model = LinearReg(model_path="test-models/test.sav")
-        lin_reg_model.preprocess(factorization_list=[],
-                                 columns=['A', 'B', 'C'],
-                                 target=['D'],
-                                 normalize_column_name='B')
+        lin_reg_model.preprocess(
+            factorization_list=[],
+            columns=["A", "B", "C"],
+            target=["D"],
+            normalize_column_name="B",
+        )
         lin_reg_model.train_model()
         # os.mkdir("test-models/")
         # lin_reg_model.evaluate()
-        #self.assertEqual(os.listdir("test-models/")[0], "test.sav")
+        # self.assertEqual(os.listdir("test-models/")[0], "test.sav")
         # shutil.rmtree("test-models")
