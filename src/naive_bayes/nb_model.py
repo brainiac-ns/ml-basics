6116e9c6293e5c8ef47b49a59934c80f4bd20675
PetarT12 <tomicafkv@gmail.com>
2023-05-25 14:50:45 +0200

    running tests 3 failed, 17 passed(ValueError: could not convert string to float: 'i love it')

--- src/naive_bayes/nb_model.py
+++ src/naive_bayes/nb_model.py
@@ -2,6 +2,12 @@ import logging
 import pandas as pd
 from base import Base
 import pickle
+import nltk
+import array
+from typing import Tuple
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from sklearn.metrics import f1_score
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
@@ -11,10 +17,18 @@ LOGGER = logging.getLogger(__name__)
 
 
 class NaiveBayes(Base):
+    """
+    A class representing a Naive Bayes classifier.
+
+    Parameters:
+    train_path: The path to the training data file.
+    labels_path: The path to the labels file.
+    model_path: The path to save the trained model.
+    """
     def __init__(
             self,
-            train_path: str = "data/text/train.tsv",
-            labels_path: str = "data/text/labels.txt",
+            train_path: str = "../data/text/train.tsv",
+            labels_path: str = "../data/text/labels.txt",
             model_path: str = "models/naivebayes.sav",
             bucket_name: str = "ml-basic",
     ):
@@ -26,64 +40,86 @@ class NaiveBayes(Base):
         LOGGER.info("Loading data")
         train_data = pd.read_csv(self.train_path, sep="\t")
         train_data["Phrase"] = train_data["Phrase"].str.lower()
-        self.X_train = train_data["Phrase"]
+        self.X_train = train_data["Phrase"].str.lower()
         self.y_train = train_data["Sentiment"]
         LOGGER.info("Splitting data into train and test sets")
-        X_train, X_test, y_train, y_test = train_test_split(
-            self.X_train, self.y_train, test_size=0.3, random_state=42
-        )
-        self.X_train = X_train
-        self.y_train = y_train
-        self.X_test = X_test
-        self.y_test = y_test
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X_train, self.y_train, test_size=0.3, random_state=42)
 
+    def preprocess(self, text: str) -> str:
+        """
+        Preprocesses the given text (using Lemmmatization and StopWord removal)
 
-    #def preprocess(self):
-        #LOGGER.info("Loading data")
-        #train_data = pd.read_csv(self.train_path, sep="\t")
+        Args:
+        text: The text to preprocess
 
-        #train_data["Phrase"] = train_data["Phrase"].str.lower()
+        Returns:
+        The preprocessed text
 
-        #X = train_data["Phrase"]
-        #y = train_data["Sentiment"]
-        #LOGGER.info("Splitting data into train and test sets")
-        #X_train, X_test, y_train, y_test 
-        #train_test_split(
-         #   X, y, test_size=0.3, random_state=42
-        #)
+        """
+        words = nltk.word_tokenize(text)
+        lemmatizer = WordNetLemmatizer()
+        lemmatized_words = [lemmatizer.lemmatize(word) for word in words]
+        stop_words = set(stopwords.words('english'))
+        filtered_words = [word for word in lemmatized_words if word.lower() not in stop_words]
+        processed_text = " ".join(filtered_words)
+        return processed_text
 
-        #return X_train, X_test, y_train, y_test
-    
-    def vectorize_data(self):
+    def vectorize_data(self) -> None:
+        """
+        Vectorizes the training and test data
+
+        """
         LOGGER.info("Vectorizing data")
         self.vectorizer = CountVectorizer()
-        self.X_train = self.vectorizer.fit_transform(self.X_train)
+        self.vectorizer.fit(self.X_train)
+        self.X_train = self.vectorizer.transform(self.X_train)
         self.X_test = self.vectorizer.transform(self.X_test)
 
-    def train(self):
+    def train(self) -> None:
+        """
+        Trains the model
+
+        """
         LOGGER.info("Training the model")
-        nb_model = self.model.fit(self.X_train, self.y_train)
-        pickle.dump(nb_model, open(self.model_path, "wb"))
+        self.model.fit(self.X_train, self.y_train)
+        pickle.dump(self.model, open(self.model_path, "wb"))
         self.upload_model(self.model_path)
 
-    def predict(self):
+    def predict(self) -> array:
+        """
+        Makes predictions on the test data
+
+        Returns:
+        Predictions of the labels
+
+        """
         LOGGER.info("Predicting")
         return self.model.predict(self.X_test)
 
-    def evaluate_model(self):
+    def evaluate_model(self) -> Tuple:
+        """
+        Evaluates the trained model
+
+        Returns:
+        A tuple containing the predicted labels and accuracy
+
+        """
         LOGGER.info("Evaluating")
-        predictions = self.predict()
+        predictions = self.model.predict(self.X_test)
         accuracy = self.model.score(self.X_test, self.y_test)
         return predictions, accuracy
 
 
 if __name__ == "__main__":
     nb = NaiveBayes()
-    #nb.preprocess()
+    LOGGER.info("Preprocess started")
+    nb.X_train = nb.X_train.apply(nb.preprocess)
+    LOGGER.info("Preprocess ended")
     nb.vectorize_data()
     nb.train()
-    predictions, accuracy = nb.evaluate_model()
+    f1, predictions, accuracy = nb.evaluate_model()
     LOGGER.info("Predictions:")
     LOGGER.info(predictions)
     LOGGER.info("Accuracy:")
     LOGGER.info(accuracy)
+
